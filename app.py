2e5b11b0aeed58ddc5f1248d28d2a6ed310a9b40
thanh97 <nguyenthanhchansol1997@gmail.com>
2021-05-29 00:16:11 +0700

    update file app new 29/05/2021

--- app.py
+++ app.py
@@ -1,54 +1,121 @@
-import tensorflow.keras
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 import cv2
-import numpy
-import os
-from keras.models import load_model
-import base64
-from keras.models import Sequential
 
-app = Flask(__name__ ,static_url_path='/static')
+import numpy as np
+from skimage import io, color, exposure, transform
+import os
+from keras import backend as K
+
+K.set_image_data_format('channels_first')
+from keras.models import load_model
+
+from datetime import datetime
+
+import joblib
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+NUM_CLASSES = 30
+IMG_SIZE = 30
+
+KERAS_MODEL = './models/my_model.h5'
 
 print("Loading model....")
-model = load_model('./models/my_model.h5',compile=False)
+model = load_model(KERAS_MODEL)
 print("....Loaded model")
+
+CLASSIFICATION_LABEL = "models/classification_labels.pkl"
+
+classification_labels = joblib.load(CLASSIFICATION_LABEL)
+classification_labels = list(classification_labels)
+
+
+def preprocess_img(img):
+    """
+    This function pre-processes the image that is given for classification. This is the same pre-processing used for input images during model training.
+
+    The images are normalized for histogram and rescaled to standard size.
+
+    INPUT: 
+        img: an input image for classification. The expected input is in png format.
+
+    OUTPUT:
+        img: the input image after pre-processing
+
+    """
+    # Extract img from object
+    if img.dtype == 'object':
+        img = img[0]
+
+    # Convert RGBA to rgB
+    if img.shape[2] == 4:
+        img = color.rgba2rgb(img)
+
+    # Resize img
+    ratio = 500.0 / img.shape[1]
+    dim = (500, int(img.shape[0] * ratio))
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    # Histogram normalization in y
+    hsv = color.rgb2hsv(img)
+    hsv[:, :, 2] = exposure.equalize_hist(hsv[:, :, 2])
+    img = color.hsv2rgb(hsv)
+
+    # central scrop
+    min_side = min(img.shape[:-1])
+    centre = img.shape[0] // 2, img.shape[1] // 2
+    img = img[centre[0] - min_side // 2:centre[0] + min_side // 2,
+          centre[1] - min_side // 2:centre[1] + min_side // 2,
+          :]
+
+    # rescale to standard size
+    img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
+
+    # roll color axis to axis 0
+    img = np.rollaxis(img, 1)
+
+    return img
+
+
+def get_class(img_path):
+    """
+    This function returns the class of the input image. 
+
+    Each image is stored within a folder that is named after its class. 
+
+
+    INPUT: 
+        img_path: path of the current image
+
+    OUTPUT:
+        The class of the image
+
+    """
+
+    return int(img_path.split('/')[-2])
 
 
 # Prediction
-def traffic_prediction(test_img, model):
-    classes = {0: 'Bien bao cam o to',
-               1: 'Bien bao cam re trai',
-               2: 'Bien bao cam re phai',
-               3: 'Bien bao cam do xe',
-               4: 'Bien bao duong khong bang phang',
-               5: 'Bien bao huong phai di vong sang trai',
-               6: 'Bien bao cam di nguoc chieu',
-               7: 'Bien bao cam xe khach va xe tai',
-               8: 'Bien bao toc do toi da cho phep',
-               9: 'Bien bao cam dung xe va do xe',
-               10: 'Bien bao het tat ca cac lenh cam',
-               11: 'Bien bao cho ngoat nguy hiem',
-               12: 'Bien bao duong giao nhau',
-               13: 'Bien bao giao nhau duong khong uu tien',
-               14: 'Bien bao giao nhau duong uu tien',
-               15: 'Bien bao noi giao nhau co tin hieu den',
-               16: 'Bien bao nguoi di bo cat ngang',
-               17: 'Bien bao tre em',
-               18: 'Bien nguoi di xe dap cat ngang',
-               19: 'Bien bao nguy hiem khac',
-               20: 'Bien bao noi giao nhau chay theo vong xuyen',
-               21: 'Bien bao cho quay xe',
-               22: 'Bien bao duong di bo',
-               23: 'Bien bao benh vien',
-               24: 'Bien bao tram xang',
-               25: 'Bien bao phan lan'
-               }
-    image = cv2.resize(test_img, (30, 30), interpolation=cv2.INTER_AREA)
-    prediction = model.predict(image.reshape(-1, 30, 30))
-    predict_img = numpy.argmax(prediction, axis=-1)
-    pred_name = classes.get(predict_img[0])
+def traffic_prediction(test_img):
+    """
+    This function returns the predicted class of the image.
 
-    return pred_name
+    INPUT: 
+        test_img: The image that has to be classified 
+
+    OUTPUT:
+        The class/prediction of the image
+
+    """
+
+    X_test = []
+    X_test.append(preprocess_img(io.imread(test_img)))
+    X_test = np.array(X_test)
+    y_pred = model.predict_classes(X_test)
+    return y_pred
 
 
 @app.route("/")
@@ -61,26 +128,60 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/api/v1', methods=['GET','POST'])
+@app.route('/api/v1', methods=['GET', 'POST'])
 def traffic_light_prediction():
     """
         This is the API for traffic sign prediction.
 
-        The image is uploaded by the user for classification. The image is saved on the server. The prediction function is called. The time taken to predict is printed as log trace.
+        The image is uploaded by the user for classification. The image is saved on the server. The prediction function is called. The time taken to predict is printed as log trace. 
         The output (predicted sign) is rendered as a page in the flask app.
     """
-    if request.method == 'POST':
-        img = request.files.getlist('image')
-        img = cv2.imdecode(numpy.fromstring(img, numpy.uint8), 1)
-        # preds = traffic_prediction(img, model)
 
-        print(img)
+    t = datetime.now()
+    # query = request.form['pic']
+    # imgFile = request.form['pic']
+
+    # print("Saving the file")
+
+    # save the image
+    imgFile = request.files['pic']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], imgFile.filename)
+
+    imgFile.save(f)
+    # print("file saved: ", f)
+    # print("uploaded image saved")
+
+    response_num = traffic_prediction(f)
+    response_label = classification_labels[int(response_num)]
+
+    dt = datetime.now() - t
+
+    print('Prediction Time (hh:mm:ss.ms) {}'.format(dt))
+    print("Image %s classified as %s" % (f, response_label))
+
+    # reply = "Welcome to this awesome page"
+    # return render_template('index.html', query=query, reply=reply)
+    return render_template('index.html', inputTrafficSign="/" + f,
+                           reply=response_label)
 
 
-    # return render_template('index.html', reply =preds)
+@app.route("/uploads/<filename>")
+def send_file(filename):
+    """
+        This function uploads the file from local/host machine to the server. 
+    """
+
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 
+@app.route("/get/<string:query>")
+def get_raw_response(query):
+    """
+        This is a placeholder function to test the app. 
+    """
+
+    return str((query))
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8050)
+    app.run(host='0.0.0.0', port=8080)
